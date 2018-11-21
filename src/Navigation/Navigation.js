d75786221e42b2da115793aab03011fdac52cedf
Magdalena Brzozowska <magdalena.brzozowska94@wp.pl>
2018-11-21 13:47:05 +0100

    made navigation sidebar

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,11 +1,23 @@
 import React from 'react'
+
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
 
 const Navigation = (props) => (
-    <AppBar
-    title="ePla"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-    />
+    <div>
+        <AppBar
+            title="ePla"
+            onLeftIconButtonClick={() => alert("clicked!")}
+        />
+
+        <Drawer
+            docked={false}
+            width={200}
+            open={true}
+            onRequestChange={(open) => console.log(open)}
+        >
+        </Drawer>
+    </div>
 )
 
 export default Navigation
